51cbdfefd09c3b9d335f034a2474924d73f6f6b6
Venkata Sai Praveen Gunda <vgunda@ncsu.edu>
2024-12-29 15:46:02 -0500

    Modified Research and Publications section

--- scripts.js
+++ scripts.js
@@ -10,3 +10,15 @@ document.querySelectorAll('nav a').forEach(anchor=>{
         smoothScroll(this.getAttribute('href'));
     });
 });
+
+document.getElementById('viewAdeecPaper').addEventListener('click',
+    function(){
+        window.open('https://ieeexplore.ieee.org/document/9936195')
+    }
+);
+
+document.getElementById('viewAssicPaper').addEventListener('click',
+    function(){
+        window.open('https://ieeexplore.ieee.org/document/10088414/')
+    }
+);
