6f2c1586da6973c756915bbe4b50a147b3d2b26f
Venkata Sai Praveen Gunda <vgunda@ncsu.edu>
2024-12-29 18:33:23 -0500

    Projects Added

--- scripts.js
+++ scripts.js
@@ -22,3 +22,30 @@ document.getElementById('viewAssicPaper').addEventListener('click',
         window.open('https://ieeexplore.ieee.org/document/10088414/')
     }
 );
+
+const buttons = document.querySelectorAll('.flipButton');
+console.log('Found ', buttons.length, ' flip buttons')
+buttons.forEach(button => {
+    button.addEventListener('click', function(event){
+            console.log('Button Clicked');
+            event.preventDefault();
+            const card = this.closest('.flip-card');
+            console.log('Found Flip Card:', card);
+            card.querySelector('.flip-card-inner').classList.toggle('is-flipped');
+        });
+    });
+
+document.querySelectorAll('.flip-card').forEach(card=>{
+    card.addEventListener('mouseleave', function(){
+        console.log('Mouse left the card');
+        const innerCard = card.querySelector('.flip-card-inner')
+        innerCard.classList.forEach(className => {
+            console.log('c -',className);
+        });
+        if(innerCard.classList.contains('is-flipped')){
+            innerCard.classList.remove('is-flipped');
+            console.log('Removed the is-flipped class')
+        }
+        
+    });
+});
